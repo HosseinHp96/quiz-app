435191f390181c7f7b73f554ace7ce49a1bfb1e4
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-06-13 18:26:14 +0330

    Set default route to quizzes route & navigate invalid routes to quizzes route

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,14 +1,22 @@
-import { RouteObject } from "react-router-dom";
+import { Navigate, RouteObject } from "react-router-dom";
 import * as Pages from "../pages";
 
 const Routes: RouteObject[] = [
   {
     path: "/",
+    element: <Navigate to="/quizzes" />,
+  },
+  {
+    path: "quizzes",
     element: <Pages.Quizzes />,
   },
   {
-    path: "/create",
+    path: "quizzes/create",
     element: <Pages.CreateQuiz />,
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" />,
   },
 ];
 
