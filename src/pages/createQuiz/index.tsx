fe4b7e8bd1e88825148b85e082e5bf67105e3b12
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-06-14 14:42:50 +0330

    Create a state to handle questions

--- src/pages/createQuiz/index.tsx
+++ src/pages/createQuiz/index.tsx
@@ -1,12 +1,23 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import { Space } from "antd";
 import AddQuestion from "./addQuestion";
+import { IQuestion } from "../../interfaces";
 
 const CreateQuiz: FC = () => {
+  const [questions, setQuestions] = useState<IQuestion[]>([]);
+
+  const addQues = (data: IQuestion) => {
+    setQuestions([data, ...questions]);
+  };
+
+  useEffect(() => {
+    console.log(questions);
+  }, [questions]);
+
   return (
     <section>
       <Space direction="vertical" size="middle" style={{ display: "flex" }}>
-        <AddQuestion />
+        <AddQuestion add={addQues} />
       </Space>
     </section>
   );
