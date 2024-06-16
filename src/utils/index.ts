0359db080a8f48f2eec333a69deb9dd411602d98
Hossein Hosseinpour <h.hosseinpour75@gmail.com>
2024-06-16 16:33:45 +0330

    Create a function to detect whether an object has an undefined value or not

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,15 @@ import { IAppContext } from "../interfaces";
 export function useAppContext() {
   return useContext(AppContext) as IAppContext;
 }
+
+export function hasUndefinedValue(obj: Record<string, unknown>): boolean {
+  for (const key in obj) {
+    if (
+      Object.prototype.hasOwnProperty.call(obj, key) &&
+      obj[key] === undefined
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
